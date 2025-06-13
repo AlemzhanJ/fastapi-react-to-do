3bcbb932cef0cd1bbf04731ba13fe5b0b4c8d318
AlemzhanJ <alemjan377@gmail.com>
2025-06-13 15:56:19 +0500

    rfrf

--- my-fastapi/app/config/db.py
+++ my-fastapi/app/config/db.py
@@ -5,7 +5,7 @@ from app.config.config import settings  # ✅ абсолютный импорт
 
 import ssl
 
-engine = create_async_engine(settings.DATABASE_URL, echo = True, connect_args={"ssl": ssl.create_default_context()})
+engine = create_async_engine(settings.DATABASE_URL, pool_pre_ping=True, echo = True, connect_args={"ssl": ssl.create_default_context()})
 
 SessionLocal = sessionmaker(bind = engine, class_ = AsyncSession, expire_on_commit = False)
 
